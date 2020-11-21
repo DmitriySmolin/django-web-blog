16d25e77eb4a244ca925aa3c6f26937e01ff9a16
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-21 14:50:42 +0400

    feat: add working with the template engine

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+news = [
+    {'title': 'Наша первая запись',
+     'text': 'Просто большой текст для 1 записи',
+     'date': '1 января 2019',
+     'author': 'Георгий'
+     },
+    {'title': 'Наша вторая запись',
+     'text': 'Просто большой текст для 2 записи',
+     'date': '10 января 2019'
+     },
+]
+
 
 def home(request):
-    return HttpResponse('<h2>Привет мир!</h2>')
+    data = {
+        'news': news,
+        'title': 'Главная страница блога'
+    }
+    return render(request, 'blog/home.html', data)
 
 
 def contacts(request):
-    return HttpResponse('<h2>Страница с контактами</h2>')
+    return render(request, 'blog/contacts.html', {'title': 'Страница про нас'})
