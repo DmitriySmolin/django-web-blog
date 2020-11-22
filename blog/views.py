e6e2b2caa1b990b88b1b8f385d5fdb6d7121af8b
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-22 22:22:06 +0400

    feat: add database creation and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import News
 
-news = [
-    {'title': 'Наша первая запись',
-     'text': 'Просто большой текст для 1 записи',
-     'date': '1 января 2019',
-     'author': 'Георгий'
-     },
-    {'title': 'Наша вторая запись',
-     'text': 'Просто большой текст для 2 записи',
-     'date': '10 января 2019'
-     },
-]
+# news = [
+#     {'title': 'Наша первая запись',
+#      'text': 'Просто большой текст для 1 записи',
+#      'date': '1 января 2019',
+#      'author': 'Георгий'
+#      },
+#
+#     {'title': 'Наша вторая запись',
+#      'text': 'Просто большой текст для 2 записи',
+#      'date': '10 января 2019'
+#      },
+# ]
 
 
 def home(request):
     data = {
-        'news': news,
+        'news': News.objects.all(),
         'title': 'Главная страница блога'
     }
     return render(request, 'blog/home.html', data)
