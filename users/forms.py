d2bfb55c5e2915bdbdf17014fe45440b73b03897
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-24 21:45:37 +0400

    feat: add changing the profile on the site

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 
 class UserOurRegistration(UserCreationForm):
@@ -9,3 +10,21 @@ class UserOurRegistration(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2', 'email']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileImg(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProfileImg, self).__init__(*args, **kwargs)
+        self.fields['img'].label = 'Изображение профиля'
+
+    class Meta:
+        model = Profile
+        fields = ['img']
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import UserOurRegistration, UserUpdateForm, ProfileImg
 
 from .forms import UserOurRegistration
+
 
 def register(request):
     if request.method == 'POST':
@@ -20,4 +22,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        img_profile = ProfileImg(request.POST, request.FILES, instance=request.user.profile)
+        update_user = UserUpdateForm(request.POST, instance=request.user)
+
+        if update_user.is_valid() and img_profile.is_valid():
+            update_user.save()
+            img_profile.save()
+            messages.success(request, f'Ваш аккаунт был успешно обновлен')
+            return redirect('profile')
+    else:
+        img_profile = ProfileImg(instance=request.user.profile)
+        update_user = UserUpdateForm(instance=request.user)
+
+    data = {
+        'img_profile': img_profile,
+        'update_user': update_user
+    }
+
+    return render(request, 'users/profile.html', data)
