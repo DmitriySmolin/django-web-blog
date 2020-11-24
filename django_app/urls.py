7c6b5779d50a59bc070a855bf10d44ff0a3cd056
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-24 12:15:07 +0400

    feat: add login and logout

--- django_app/urls.py
+++ django_app/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     # path('register/', include('users.urls')),
-    path('reg/', user_views.register, name='reg')
+    path('reg/', user_views.register, name='reg'),
+    path('profile/', user_views.profile, name='profile'),
+    path('auth/', auth_views.LoginView.as_view(template_name='users/auth.html'), name="auth"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
