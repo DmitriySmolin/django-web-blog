fb06161ed77cfde7b631e44f5ced63508e76e830
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-24 10:30:51 +0400

    feat: add registration on the site

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
 from .models import News
 
-# news = [
-#     {'title': 'Наша первая запись',
-#      'text': 'Просто большой текст для 1 записи',
-#      'date': '1 января 2019',
-#      'author': 'Георгий'
-#      },
-#
-#     {'title': 'Наша вторая запись',
-#      'text': 'Просто большой текст для 2 записи',
-#      'date': '10 января 2019'
-#      },
-# ]
 
 
 def home(request):
