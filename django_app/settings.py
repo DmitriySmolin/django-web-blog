606f384f609b5d92230c5af63c8b9904f9e40e53
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-11-29 16:12:33 +0400

    feat: add password recovery button

--- django_app/settings.py
+++ django_app/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +118,13 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
 MEDIA_URL = '/pictures/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'neox56@gmail.com'
+EMAIL_HOST_PASSWORD = 'ousrrfqijgmlgdxi'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
--- django_app/urls.py
+++ django_app/urls.py
@@ -27,6 +27,21 @@ urlpatterns = [
     path('reg/', user_views.register, name='reg'),
     path('profile/', user_views.profile, name='profile'),
     path('auth/', auth_views.LoginView.as_view(template_name='users/auth.html'), name="auth"),
+
+    path('pass-reset/', auth_views.PasswordResetView.as_view(template_name='users/pass_reset.html'), name="pass-reset"),
+
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name="password_reset_confirm"),
+
+    path('password_reset_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name="password_reset_complete"),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name="password_reset_done"),
+
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
 
